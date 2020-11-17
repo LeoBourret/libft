29369e0e2d9cad97fc61fa0e3ec1e06dddf4effe
LeoBourret <bourretleo@hotmail.fr>
2020-11-17 17:29:40 +0100

    wesh

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lebourre <lebourre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:22:56 by lebourre          #+#    #+#             */
-/*   Updated: 2020/11/16 18:22:57 by lebourre         ###   ########.fr       */
+/*   Updated: 2020/11/17 17:29:30 by lebourre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t			i;
 	unsigned char	*ptr;
 
+	if (!src || !dst)
+		return (NULL);
 	ptr = (unsigned char *)dst;
 	i = -1;
 	while (++i < n)
