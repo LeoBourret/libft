eee423faf89b63832ede8b41a78b38043d748b6e
LeoBourret <bourretleo@hotmail.fr>
2020-11-17 20:25:43 +0100

    l

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lebourre <lebourre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:24:00 by lebourre          #+#    #+#             */
-/*   Updated: 2020/11/17 20:06:19 by lebourre         ###   ########.fr       */
+/*   Updated: 2020/11/17 20:25:28 by lebourre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 	size_t j;
 
 	i = -1;
-	if (len == 0)
-		return (NULL);
+	if (*s2 == '\0' || !s2)
+		return ((char*)s1);
 	while (s1[++i] && i < len)
 	{
 		j = 0;
