bcf5a0963b204ab4fe121e5535099190465abf47
LeoBourret <bourretleo@hotmail.fr>
2020-11-17 19:34:39 +0100

    l

--- ft_lstmap_save.c
+++ ft_lstmap_save.c
@@ -6,29 +6,29 @@
 /*   By: lebourre <lebourre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:22:35 by lebourre          #+#    #+#             */
-/*   Updated: 2020/11/17 19:34:30 by lebourre         ###   ########.fr       */
+/*   Updated: 2020/11/17 19:25:31 by lebourre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_lst;
-	t_list	*new_elem;
+	t_list *new_elem;
+	t_list *new_list;
 
 	if (!f || !del)
 		return (NULL);
-	new_lst = NULL;
+	new_list = NULL;
 	while (lst)
 	{
-		if (!(new_elem = ft_lstnew(f(lst->content))))
+		if (!(new_elem = ft_lstnew((*f)(lst->content))))
 		{
-			ft_lstclear(&new_lst, del);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_lst, new_elem);
+		ft_lstadd_back(&new_list, new_elem);
 		lst = lst->next;
 	}
-	return (new_lst);
+	return (new_list);
 }
